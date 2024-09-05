84b12a63cd412ea4da16afcd1f6c6361db83c591
Anahit Amyan <136964764+anAmyan02@users.noreply.github.com>
2024-09-05 21:28:10 +0400

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\AdminController;
+// use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+ 
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+ 
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
+// Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    // Route::get('/profile', [UserController::class, 'userprofile'])->name('profile');
+// });
