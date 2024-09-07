fba5e9fe8b872fda4c1bf32acf560c8e8cda1370
Anahit Amyan <136964764+anAmyan02@users.noreply.github.com>
2024-09-07 19:03:54 +0400

    add forgot-pass

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,8 @@
+<form method="POST" action="{{ route('password.email') }}">
+    @csrf
+    <div>
+        <label for="email">Email Address</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+    </div>
+    <button type="submit">Send Password Reset Link</button>
+</form>
